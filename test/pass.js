ea689e50e2038e5f688392495cc0a63ae9d81560
Robin Berjon <robin@berjon.com>
2013-12-19 16:26:14 +0100

    have Sink really work

--- test/pass.js
+++ test/pass.js
@@ -8,6 +8,12 @@ var validator = require("../lib/node-validator").makeSpecberus()
 ,   profiles = "dummy".split(" ")
 ;
 
+function Sink () {
+    this.ok = false;
+    this.done = 0;
+}
+util.inherits(Sink, events.EventEmitter);
+
 profiles.forEach(function (profileName) {
     var profile = require("../lib/profiles/" + profileName);
     describe("Gives a pass to all the files in profile: " + profileName, function (allDone) {
@@ -17,8 +23,7 @@ profiles.forEach(function (profileName) {
         fs.readdirSync(testDir).forEach(function (file) {
             total++;
             it("should pass for file: " + file, function (done) {
-                var sink = { ok: false, done: 0 };
-                util.inherits(sink, events.EventEmitter);
+                var sink = new Sink;
                 sink.on("ok", function () { this.ok = true; });
                 sink.on("done", function () {
                     expect(this.ok).to.be.ok();
