0cdc48b6d37594d1e2e8b4377c71872f2174bf68
tripu <tripu@tripu.info>
2015-04-28 12:22:54 +0900

    Fix coding style (comma at the beginning of line).

--- test/all-rules.js
+++ test/all-rules.js
@@ -164,8 +164,8 @@ var tests = {
 ,   heuristic:   {
         'date-format':  [
             { doc: "heuristic/dates.html" }
-        ,   { doc: "heuristic/bad-dates.html", errors: ['heuristic.date-format', 'heuristic.date-format', 'heuristic.date-format'] },
-            { doc: "heuristic/dated-url.html" }
+        ,   { doc: "heuristic/bad-dates.html", errors: ['heuristic.date-format', 'heuristic.date-format', 'heuristic.date-format'] }
+        ,   { doc: "heuristic/dated-url.html" }
         ]
     }
 };
