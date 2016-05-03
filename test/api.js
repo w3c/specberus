a16a84e0d805969bcac365360e0575a2bf08348a
tripu <t@tripu.info>
2016-05-03 16:59:23 +0200

    REST API: finish writing tests

--- test/api.js
+++ test/api.js
@@ -2,63 +2,132 @@
  * Test the REST API.
  */
 
+// Settings:
+const DEFAULT_PORT = 8000
+,   PORT = process.env.PORT || DEFAULT_PORT
+,   ENDPOINT = 'http://localhost:' + PORT + '/api/'
+,   TIMEOUT = 30000
+;
+
+// Native packages:
+const http = require('http');
+
 // External packages:
-const expect = require('expect.js')
+const chai = require('chai')
+,   chaiAsPromised = require('chai-as-promised')
+,   express = require('express')
 ,   superagent = require('superagent')
 ;
 
 // Internal packages:
-const samples = require('./samples')
-,   package = require('../package')
+const package = require('../package')
+,   api = require('../lib/api')
+;
+
+var expect
+,   server
 ;
 
 /**
- * Assert that the profile detected in a spec is equal to the known profile.
- *
- * @param {String} url - public URL of a spec.
- * @param {String} profile - profile that should be detected.
+ * Launch an HTTP server for tests.
  */
 
-const detect = function(url, profile) {
-    it('Should detect a ' + profile, function () {
-        // @TODO; submit URL to endpoint and check profiles.
+const launchServer = function () {
+    const app = express();
+    server = http.createServer(app);
+    api.setUp(app);
+    server.listen(PORT).on('error', function(err) {
+        throw new Error(err);
+    });
+};
+
+/**
+ * Set up the testing framework.
+ */
+
+const setUp = function() {
+    chai.use(chaiAsPromised);
+    expect = chai.expect;
+};
+
+/**
+ * Query the API.
+ */
+
+const get = function (suffix, post) {
+    const method = post ? superagent.post : superagent.get;
+    return new Promise(function (resolve, reject) {
+        method(ENDPOINT + suffix, {
+            timeout: TIMEOUT,
+            encoding: null
+        }, function (error, response, body) {
+            if (error) {
+                if (error.response && error.response.error && error.response.error.text)
+                    reject(new Error(error.response.error.text));
+                else
+                    reject(new Error('Fetching “' + ENDPOINT + suffix + '” triggered a network error: ' + error.message));
+            }
+            else if (response.statusCode !== 200)
+                reject(new Error('Fetching “' + ENDPOINT + suffix + '” triggered an HTTP error: code ' + response.statusCode));
+            else if (response.res && response.res.text)
+                resolve(response.res.text);
+            else
+                resolve(body);
+        });
     });
 };
 
 if (!process || !process.env || !process.env.SKIP_NETWORK) {
 
-    // @TODO: launch Specberus locally as a server, listening to HTTP requests.
-
     describe('API', function() {
 
-/*        it('The endpoint should exist', function() {
-            // @TODO
+        var suffix;
+
+        before(function() {
+            launchServer();
+            setUp();
+            suffix = '?file=test/docs/metadata/ttml-imsc1.html'
         });
 
-        describe('Method "version"', function() {
-            it('Should exist'), function() {
-                // @TODO
-            };
-            it('Should return the right version string'), function() {
-                // @TODO; query method and compare with "package.version".
-            };
+        describe('Endpoint', function() {
+            it('Should exist and listen to GET requests', function() {
+                const bogus = get('');
+                return expect(bogus).to.eventually.be.rejectedWith(/wrong\ api\ method/i);
+            });
+            it('Should not accept POST requests', function() {
+                const bogus = get('', true);
+                return expect(bogus).to.eventually.be.rejectedWith(/cannot\ post/i);
+            });
         });
 
-        describe('Method "metadata"', function() {
-            it('Should exist'), function(done) {
-                // @TODO
-            };
-            for(var i in samples) {
-                detect(samples[i].url, samples[i].profile);
-            }
+        describe('Method “version”', function() {
+            it('Should return the right version string', function() {
+                const version = get('version');
+                return expect(version).to.eventually.become(package.version);
+            });
         });
 
-        describe('Method "validate"', function() {
-            it('Should exist'), function(done) {
-                // @TODO
-            };
-            // @TODO; submit a few sample specs for validation; check results.
-        }); */
+        describe('Method “metadata”', function() {
+            it('Should return the expected profile', function() {
+                const meta = get('metadata' + suffix);
+                return expect(meta).to.eventually.match(/"profile":\s*"pr"/i);
+            });
+        });
+
+        describe('Method “validate”', function() {
+            it('Should 404 and return an array of errors when validation fails', function() {
+                const check = get('validate' + suffix + '&profile=REC&validation=simple-validation&processDocument=2047');
+                return expect(check).to.eventually.be.rejectedWith(/"headers\.\w+/i);
+            });
+            it('Should succeed and return the profile when the document is valid', function() {
+                const check = get('validate' + suffix + '&profile=PR&validation=simple-validation&processDocument=2015');
+                return expect(check).to.eventually.become('PR');
+            });
+        });
+
+        after(function() {
+            server.close();
+        });
 
     });
 
