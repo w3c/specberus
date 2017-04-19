538e9ce816e57d7e6811ab9eef5a01ddee246c3f
tripu <t@tripu.info>
2017-04-19 14:54:51 +0200

    Fix #502

--- public/js/scroll-tweak.js
+++ public/js/scroll-tweak.js
@@ -5,7 +5,7 @@
 $(document).ready(function() {
     if (document.location && document.location.hash && window.pageYOffset && window.innerHeight)
         window.setTimeout(function() {
-            const elem = $(document.location.hash);
+            var elem = $(document.location.hash);
             if (elem && elem.length > 0) elem[0].scrollIntoViewIfNeeded();
             window.setTimeout(function() {
                 window.scrollTo(window.pageXOffset, window.pageYOffset - window.innerHeight * 0.5);
