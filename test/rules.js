efcd8668ac6afa55036638b4cada7bd90ae020b6
jennyliang <jiaojiaomao220@163.com>
2020-06-17 12:16:31 +0800

    fix test for metadata

--- test/rules.js
+++ test/rules.js
@@ -425,12 +425,6 @@ var tests = {
 ,   validation: validation
 };
 
-// var tests = {
-//     // Categories
-//     sotd:   {
-//     }
-// }
-
 Object.keys(tests).forEach(function (category) {
     describe("Category " + category, function () {
         Object.keys(tests[category]).forEach(function (rule) {
