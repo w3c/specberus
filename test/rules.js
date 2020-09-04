5a2192b23fd7f17d4ba252fdca16db577db46350
jennyliang <jiaojiaomao220@163.com>
2020-09-04 15:35:42 +0800

    fix test

--- test/rules.js
+++ test/rules.js
@@ -200,16 +200,16 @@ var tests = {
         ]
     ,   dl:  [
             { doc: "headers/simple.html", config: { previousVersion: true, status: "WD" }, errors: ["headers.dl.cant-retrieve"] }
-        ,   { doc: "headers/fails.html", errors: ["headers.dl.this-version", "headers.dl.latest-version", "headers.dl.implelink-not-found"] }
-        ,   { doc: "headers/fails.html", config: { previousVersion: true }, errors: ["headers.dl.this-version", "headers.dl.latest-version", "headers.dl.previous-version", "headers.dl.implelink-not-found"] }
+        ,   { doc: "headers/fails.html", config: {status: "REC" }, errors: ["headers.dl.this-version", "headers.dl.latest-version", "headers.dl.implelink-not-found"] }
+        ,   { doc: "headers/fails.html", config: {status: "REC", previousVersion: true }, errors: ["headers.dl.this-version", "headers.dl.latest-version", "headers.dl.previous-version", "headers.dl.implelink-not-found"] }
         ,   { doc: "headers/dl-order.html", errors: ["headers.dl.this-latest-order", "headers.dl.latest-previous-order", "headers.dl.cant-retrieve", "headers.dl.implelink-should-be-https", "headers.dl.editors-draft-should-be-https"], warnings: ["headers.dl.previous-not-needed"] }
-        ,   { doc: "headers/dl-mismatch.html"
+        ,   { doc: "headers/dl-mismatch.html", config: { status: "REC" }
             , errors: ["headers.dl.this-link", "headers.dl.this-syntax", "headers.dl.latest-link", "headers.dl.latest-syntax", "headers.dl.previous-link", "headers.dl.previous-syntax", "headers.dl.implelink-not-found"]
             , warnings: ["headers.dl.previous-not-needed"] }
-        ,   { doc: "headers/wrong-urls.html", errors: ["headers.dl.previous-syntax", "headers.dl.cant-retrieve", "headers.dl.implelink-not-found"], config: { previousVersion: true, status: "WD" } }
-        ,   { doc: "headers/dl-trailing-whitespace.html", config: { previousVersion: true, status: "WD" }, errors: ["headers.dl.cant-retrieve", "headers.dl.implelink-not-found"] }
-        ,   { doc: "headers/dl-untrimmed-text.html", config: { previousVersion: true, status: "WD" }, errors: ["headers.dl.cant-retrieve", "headers.dl.implelink-not-found"] }
-        ,   { doc: "headers/shortnameChange.html", config: { previousVersion: true, status: "WD" }, warnings: ["headers.dl.this-previous-shortname", "headers.dl.implelink-not-found"] }
+        ,   { doc: "headers/wrong-urls.html", errors: ["headers.dl.previous-syntax", "headers.dl.cant-retrieve"], config: { previousVersion: true, status: "WD" } }
+        ,   { doc: "headers/dl-trailing-whitespace.html", config: { previousVersion: true, status: "WD" }, errors: ["headers.dl.cant-retrieve"] }
+        ,   { doc: "headers/dl-untrimmed-text.html", config: { previousVersion: true, status: "WD" }, errors: ["headers.dl.cant-retrieve"] }
+        ,   { doc: "headers/shortnameChange.html", config: { previousVersion: true, status: "WD" }, warnings: ["headers.dl.this-previous-shortname"] }
         ,   { doc: "headers/wg-note.html", config: { previousVersion: true, status: "NOTE" }, errors: ["headers.dl.previous-version"] }
         ,   { doc: "headers/wg-note.html", config: { status: "NOTE" } }
         ,   { doc: "headers/seriesShortlink.html", config: { previousVersion: true, status: "WD" }, errors: ["headers.dl.cant-retrieve"] }
@@ -313,7 +313,7 @@ var tests = {
         ,   { doc: "sotd/rec-obsl.html", errors: ["sotd.rec-comment-end.not-found"] }
         ,   { doc: "sotd/rec-rescind.html", warnings: ["sotd.rec-comment-end.multi-found"] }
         ]
-    ,   "cr-intro": [
+    ,   "publish": [
             { doc: "sotd/cr-end.html", config: { status: "CR", crType: "Snapshot"}}
         ,   { doc: "sotd/cr-end-27days.html", config: { status: "CRD", crType: "Draft" }}
         ]
@@ -326,15 +326,15 @@ var tests = {
             { doc: "headers/simple.html" }
         ,   { doc: "sotd/supersedable.html", errors: ["sotd.supersedable.no-sotd-intro", "sotd.supersedable.no-sotd-tr"] }
         ]
-    ,   "mailing-list":  [
-            { doc: "headers/simple.html" }
-        ,   { doc: "sotd/ml-bad.html"
-            , errors: ["headers.mailing-list.no-list", "headers.mailing-list.no-arch"]
-            , warnings: ["headers.mailing-list.no-sub"]
-            }
-        ,   { doc: "sotd/ml-missing.html" }
-        ,   { doc: "headers/simple.html", config: { status: "REC" }, errors: ["headers.mailing-list.no-repo"] }
-        ]
+    // ,   "mailing-list":  [
+    //         { doc: "headers/simple.html" }
+    //     ,   { doc: "sotd/ml-bad.html"
+    //         , errors: ["headers.mailing-list.no-list", "headers.mailing-list.no-arch"]
+    //         , warnings: ["headers.mailing-list.no-sub"]
+    //         }
+    //     ,   { doc: "sotd/ml-missing.html" }
+    //     ,   { doc: "headers/simple.html", config: { status: "REC" }, errors: ["headers.mailing-list.no-repo"] }
+    //     ]
     ,   pp:  [
             { doc: "sotd/pp-20170801.html", config: { recTrackStatus: true }, errors: ["sotd.pp.no-pp", "sotd.pp.no-claims", "sotd.pp.no-section6"] }
         ,   { doc: "headers/simple.html", config: { recTrackStatus: true }, errors: ["sotd.pp.no-pp"] }
