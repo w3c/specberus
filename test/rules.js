6ff6f86324a097f876cdcfd89cffabbc05456165
jennyliang <jiaojiaomao220@163.com>
2021-11-04 15:06:29 +0800

    add patch for pp check

--- test/rules.js
+++ test/rules.js
@@ -944,6 +944,7 @@ const tests = {
                     patentPolicy: 'pp2004',
                     track: 'Note',
                 },
+                errors: ['sotd.pp.no-pp'],
             },
             {
                 doc: 'headers/wg-note1.html',
