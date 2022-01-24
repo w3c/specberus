f7b66cb63ecf59fc7682d333c3033e68bfa5b884
JiayingL <jennyliang@w3.org>
2022-01-24 15:04:55 +0800

    Update test/data/TR/Recommendation/FPWD.js
    Co-authored-by: Denis Ah-Kang <deniak@users.noreply.github.com>

--- test/data/TR/Recommendation/FPWD.js
+++ test/data/TR/Recommendation/FPWD.js
@@ -491,14 +491,6 @@ exports.rules = {
                 },
                 errors: ['sotd.pp.no-pp2020'],
             },
-            // TODO: find a patent neither pp2020 pp2017
-            // {
-            //     data: 'noPPLink',
-            //     config: {
-            //         patentPolicy: 'pp2021',
-            //     },
-            //     errors: ['sotd.pp.no-pp-link'],
-            // },
             {
                 data: 'noDisclosures',
                 config: {
