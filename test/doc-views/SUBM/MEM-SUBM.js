0a37149423695725d1dfe9e420d6b2d16a0b6bd5
jennyliang <jiaojiaomao220@163.com>
2023-04-11 16:57:29 +0800

    change copyright test files

--- test/doc-views/SUBM/MEM-SUBM.js
+++ test/doc-views/SUBM/MEM-SUBM.js
@@ -35,7 +35,7 @@ export default {
         noCopyright: {
             ...good,
             copyright: {
-                show: false,
+                showDefault: false,
             },
         },
         noCopyrightLink: {
