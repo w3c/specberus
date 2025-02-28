407c35491d32e5f3fe8baf44ece652bfa5a2cd3d
Denis Ah-Kang <denis@w3.org>
2025-02-28 15:40:48 +0400

    check errors

--- test/doc-views/TR/Recommendation/REC.js
+++ test/doc-views/TR/Recommendation/REC.js
@@ -179,7 +179,7 @@ export default {
         },
     },
     translation: {
-        notFound: {
+        noTranslation: {
             ...good,
             config: {
                 ...good.config,
