49f3f98a4e3f724bbcacfcdc86feeaa59f2a10bb
Kenneth G. Franqueiro <kfranqueiro@users.noreply.github.com>
2026-05-06 07:44:04 -0400

    Migrate files under lib to TypeScript (#2090)
    Co-authored-by: Denis Ah-Kang <1696128+deniak@users.noreply.github.com>

---
+++ lib/rules/echidna/deliverer-change.ts
@@ -0,0 +1,60 @@
+// @ts-ignore (no typings)
+import w3cApi from 'node-w3capi';
+import type { RuleCheckFunction, RuleMeta } from '../../types.js';
+
+const self: RuleMeta = {
+    name: 'echidna.deliverer-change',
+    section: 'metadata',
+    rule: 'delivererID',
+};
+
+export const { name } = self;
+
+async function getPreviousDelivererIDs(
+    shortname: string,
+    previousUrl: string
+): Promise<number[]> {
+    const date = previousUrl.match(/-(\d{8})\/$/);
+    if (!date) return Promise.resolve([]);
+
+    // e.g. https://api.w3.org/specifications/WGSL/versions/20230221/deliverers
+    const data: { id: number }[] = await w3cApi
+        .specification(shortname)
+        .version(date[1])
+        .deliverers()
+        .fetch({ embed: true })
+        .catch(() => []);
+    return data.map(({ id }) => id);
+}
+
+/**
+ * @param sr
+ * @param done
+ */
+export const check: RuleCheckFunction = async (sr, done) => {
+    const previousVersion = await sr.getPreviousVersion();
+    const shortname = await sr.getShortname();
+
+    if (!previousVersion || !shortname) {
+        return done();
+    }
+
+    const previousDelivererIDs = await getPreviousDelivererIDs(
+        shortname,
+        previousVersion
+    );
+    const delivererIDs = await sr.getDelivererIDs();
+
+    const delivererChanged =
+        delivererIDs.sort().toString() !==
+        previousDelivererIDs.sort().toString();
+
+    if (delivererChanged) {
+        sr.error(self, 'deliverer-changed', {
+            this: delivererIDs.sort().toString(),
+            previous: previousDelivererIDs.sort().toString(),
+        });
+    }
+
+    done();
+};
---
+++ lib/rules/sotd/supersedable.ts
@@ -0,0 +1,47 @@
+// SotD
+//  must start with
+//      <p><em>This section describes the status of this document at the time of its publication.
+//      A list of current W3C publications and the
+//      latest revision of this technical report can be found in the
+//      <a href="https://www.w3.org/TR/">W3C Standards and draft index</a>.</em></p>
+
+import type { RuleCheckFunction, RuleMeta } from '../../types.js';
+
+const self: RuleMeta = {
+    name: 'sotd.supersedable',
+    section: 'document-status',
+    rule: 'boilerplateTRDoc',
+};
+
+export const { name } = self;
+
+export const check: RuleCheckFunction = (sr, done) => {
+    const $sotd = sr.getSotDSection();
+    if ($sotd) {
+        let $em = $sotd.filter('p').children('em').first();
+        if (!$em.length) $em = $sotd.find('p em').first();
+        const txt = sr.norm($em.text());
+        const wanted = `${'This section describes the status of this document at the time of its publication. A list of current W3C publications '}${
+            sr.config!.status === 'SUBM'
+                ? ''
+                : 'and the latest revision of this technical report '
+        }can be found in the W3C standards and drafts index.`;
+
+        const deprecatedWanted = wanted.replace(
+            'W3C standards and drafts index',
+            'W3C technical reports index'
+        );
+
+        if (txt !== wanted) {
+            if (txt === deprecatedWanted) {
+                sr.warning(self, 'deprecated');
+            } else {
+                sr.error(self, 'no-sotd-intro');
+            }
+        }
+
+        const $a = $em.find("a[href='https://www.w3.org/TR/']");
+        if (!$a.length) sr.error(self, 'no-sotd-tr');
+    }
+    done();
+};
