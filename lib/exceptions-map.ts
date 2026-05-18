62dc150e8fdf4ec837bf83f120d9b5fe6dc24fcf
Kenneth G. Franqueiro <kfranqueiro@users.noreply.github.com>
2026-05-18 13:52:43 -0400

    Convert JSON files to TypeScript modules (#2097)

---
+++ lib/exceptions-map.ts
@@ -0,0 +1,64 @@
+export interface Exception {
+    rule: string;
+    message?: RegExp;
+    type?: string;
+}
+
+export const exceptions = new Map<RegExp, Exception[]>([
+    [
+        /.*/,
+        [
+            {
+                rule: 'validation.html',
+                type: 'noexistence-at-all',
+                message: /^CSS: .*$/,
+            },
+            {
+                rule: 'validation.html',
+                message:
+                    /Bad value “publication” for attribute “rel” on element “link”: The string “publication” is not a registered keyword./,
+            },
+        ],
+    ],
+    [
+        /^audiobooks$/,
+        [
+            {
+                rule: 'sotd.candidate-review-end',
+            },
+        ],
+    ],
+    [
+        /^did-core$/,
+        [
+            {
+                rule: 'heuristic.date-format',
+            },
+        ],
+    ],
+    [
+        /^privacy-principles$/,
+        [
+            {
+                rule: 'headers.editor-participation',
+            },
+        ],
+    ],
+    [
+        /^pub-manifest$/,
+        [
+            {
+                rule: 'sotd.candidate-review-end',
+            },
+        ],
+    ],
+    [
+        /^webgpu|WGSL$/,
+        [
+            {
+                rule: 'headers.dl',
+                message: /Implementation report/,
+            },
+        ],
+    ],
+]);
